b652e99f022648346209ea3a86d5d6ff6185e3a4
Patrick Hogan <phogan110@live.com>
2018-12-24 17:09:32 -0700

    set up `app` call for express; set up express server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,9 @@
-const express = require("express");
+const express = require("express"); //import express
+
+let app = express(); //call express with app
+
+app.get("/", (req, res) => {
+  res.send("Hello from the web server side...");
+}); //respond to get req by sending text
+
+app.listen(3000); //listen on port 3000
