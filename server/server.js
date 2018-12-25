c85c9ad424c99ba8883186880e81d963a8420b45
Patrick Hogan <phogan110@live.com>
2018-12-24 17:26:51 -0700

    commented out .get code; added middleware

--- server/server.js
+++ server/server.js
@@ -4,9 +4,15 @@ const fs = require("fs");
 
 let app = express(); //call express with app
 
-app.get("/", (req, res) => {
-  res.send("Hello from the web server side...");
-}); //respond to get req by sending text
+// app.get("/", (req, res) => {
+//   res.send("Hello from the web server side...");
+// }); //respond to get req by sending text
+
+//middleware logger
+app.use((req, res, next) => {
+  console.log(req.originalUrl);
+  next();
+});
 
 app.use(express.static(path.join(__dirname, "../public"))); //tells `express` to serve all files in the public directory
 
