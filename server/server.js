2c092e905bd9e5faa226f90696815be1ae6080e4
Patrick Hogan <phogan110@live.com>
2018-12-24 17:15:19 -0700

    used express.static to serve all files in /public

--- server/server.js
+++ server/server.js
@@ -6,4 +6,6 @@ app.get("/", (req, res) => {
   res.send("Hello from the web server side...");
 }); //respond to get req by sending text
 
+app.use(express.static(path.join(__dirname, "../public"))); //tells `express` to serve all files in the public directory
+
 app.listen(3000); //listen on port 3000
