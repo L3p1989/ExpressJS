fe79b8ddda3ae20d7e75de7783f9e99fcd2de4ab
Patrick Hogan <phogan110@live.com>
2018-12-26 13:04:35 -0700

    commented old post. made new post to read file after submitting

--- server/server.js
+++ server/server.js
@@ -17,15 +17,37 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// app.post("/formsubmission", (req, res) => {
+//   let firstName = req.body.firstName;
+//   let lastName = req.body.lastName;
+//   let formData = {
+//     firstName: firstName,
+//     lastName: lastName
+//   }; //adds submitted firstName and lastName to formData
+//   // let formJSON = JSON.stringify(formData); //converts formData to a JSON object
+//   // fs.appendFileSync("form-submission.JSON", formJSON); //appends submission to form-submission
+// });
+
 app.post("/formsubmission", (req, res) => {
   let firstName = req.body.firstName;
   let lastName = req.body.lastName;
   let formData = {
-    firstName: firstName,
-    lastName: lastName
-  }; //adds submitted firstName and lastName to formData
-  let formJSON = JSON.stringify(formData); //converts formData to a JSON object
-  fs.appendFileSync("form-submission.JSON", formJSON); //appends submission to form-submission
+    firstName,
+    lastName
+  };
+  fs.readFile(path.join(__dirname, "../form-submission.json"), (err, data) => {
+    if (err) console.log(err);
+    let parsedData = JSON.parse(data);
+    parsedData.push(formData);
+    fs.writeFile(
+      path.join(__dirname, "../form-submission.json"),
+      JSON.stringify(parsedData),
+      err => {
+        if (err) console.log(err);
+        res.send(parsedData);
+      }
+    );
+  });
 });
 
 app.use(express.static(path.join(__dirname, "../public"))); //tells `express` to serve all files in the public directory
